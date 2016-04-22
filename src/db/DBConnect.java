37a0dd1e4f7253835fddfa352447aa48310e3cb2
YJS <aragaki7@gmail.com>
2016-04-22 10:50:09 +0900

    dbconnect

---
+++ src/db/DBConnect.java
@@ -0,0 +1,24 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnect {
+	private static final String JDBC = "jdbc:mysql://133.130.88.202/hanbit";
+	private static final String ID = "scott";
+	private static final String PW = "tiger";
+	private static final String FORNAME = "com.mysql.jdbc.Driver";
+	private static Connection connection;
+
+	public static Connection get() {
+		try {
+			Class.forName(FORNAME);
+			connection = DriverManager.getConnection(JDBC, ID, PW);
+		} catch (Exception e) {
+			e.getMessage();
+		}
+
+		return connection;
+	}
+
+}
