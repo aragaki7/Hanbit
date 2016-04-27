76822762cb85378e9b7ea814533b5467f8eda10b
Administrator <Administrator@WIN-2N04TK7ABLN>
2016-04-27 14:31:17 +0900

    list 슈정

--- src/com/hb/controller/attend/ListAttendController.java
+++ src/com/hb/controller/attend/ListAttendController.java
@@ -7,7 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-	@WebServlet("/attend/list.do")
+
 	public class ListAttendController extends HttpServlet{
 		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 			request.getRequestDispatcher("/attend/list.jsp").forward(request, response);
