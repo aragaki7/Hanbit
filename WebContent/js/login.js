8a17159075a9c85969cd0254ff78176ee2f972ed
hb6007 <hb6007@hb6007>
2016-04-27 12:11:02 +0900

    로긴 엔터키 먹음

--- WebContent/js/login.js
+++ WebContent/js/login.js
@@ -1,32 +1,42 @@
-	$(document).ready(function(){
-		$(".login>button").click(function(){
-			var arr =[];
-			$(".login>input").each(function(idx, item){
-				arr[idx]=$(item).val();
-			});
-			$.ajax({
-				"url":"/Hanbit/login.do",
-				"data":{"id":arr[0],"pw":arr[1]},
-				"method":"POST",
-//				"datatype":"json",
-				"success":function(data){
-					var obj = JSON.parse(data);
-					if(obj.result=='success'){
-						alert(obj.result);
-						var st = "";
-						st+= "<div style=\"margin-left:10px;\" id=\"form\"><div class=\"login\"><div style=\"font-size: 15px;\"><h3>"+obj.id+"님 환영합니다.</h3></div>"
-							+"<div><a href=\"/Hanbit/logout.do\"><span>logout</span></a><a href=\"/Hanbit/login/edituser.jsp\"><span>회원정보수정</span></a>"
-							+"</div></div></div>";
-						$(".login").html(st);
-						$('#log').remove()
-					}else{
-						alert(obj.result);
-						alert('아이디 비번 확인');
-					}
-				},
-				"error":function(){
-					alert("error");					
+var loginfuc=function(){
+	var arr =[];
+	$(".login>input").each(function(idx, item){
+		arr[idx]=$(item).val();
+		});
+		$.ajax({
+			"url":"/Hanbit/login.do",
+			"data":{"id":arr[0],"pw":arr[1]},
+			"method":"POST",
+//			"datatype":"json",
+			"success":function(data){
+				var obj = JSON.parse(data);
+				if(obj.result=='success'){
+					alert(obj.result);
+					var st = "";
+					st+= "<div style=\"margin-left:10px;\" id=\"form\"><div class=\"login\"><div style=\"font-size: 15px;\"><h3>"+obj.id+"님 환영합니다.</h3></div>"
+						+"<div><a href=\"/Hanbit/logout.do\"><span>logout</span></a><a href=\"/Hanbit/login/edituser.jsp\"><span>회원정보수정</span></a>"
+						+"</div></div></div>";
+					$(".login").html(st);
+					$('#log').remove();
+				}else{
+					alert(obj.result);
+					alert('아이디 비번 확인');
 				}
-			});
+			},
+			"error":function(){
+				alert("error");					
+			}
+		});
+		};
+
+$(document).ready(function(){
+		$(".login>button").click(function(){
+			loginfuc();
+		});
+		
+		$('input[type=password]').keydown(function(e){
+			if (e.keyCode == 13) {
+				loginfuc();
+			}
 		});
 	});
