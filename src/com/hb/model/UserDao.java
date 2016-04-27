f9d1caed8497e9682de0b9a3331554da0aff8f5f
hb6007 <hb6007@hb6007>
2016-04-27 14:37:00 +0900

    dao 기본 틀 만듬

--- src/com/hb/model/UserDao.java
+++ src/com/hb/model/UserDao.java
@@ -1,8 +1,18 @@
 package com.hb.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import db.DBConnect;
 public class UserDao {
-
-	public static void main(String[] args) {
+	private Connection conn;
+	private Statement stmt;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	public UserDao() {
+		conn = DBConnect.get();
 	}
-
 }
