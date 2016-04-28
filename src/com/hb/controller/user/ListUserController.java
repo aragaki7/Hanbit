ec58c120e358514edda978185df4f4c6414c3112
hb6008 <hb6008@hb6008>
2016-04-28 14:45:35 +0900

    학생정보가져오기

--- src/com/hb/controller/user/ListUserController.java
+++ src/com/hb/controller/user/ListUserController.java
@@ -19,8 +19,8 @@ public class ListUserController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
 		UserDao dao = new UserDao();
-		ArrayList<UserData> stulist = dao.Userlist(1);
-		ArrayList<UserData> tealist = dao.Userlist(2);
+		ArrayList<UserData> stulist = dao.Userlist(2);
+		ArrayList<UserData> tealist = dao.Userlist(5);
 		request.setAttribute("stulist", stulist);
 		request.setAttribute("tealist", tealist);
 		request.getRequestDispatcher("/user/list.jsp").forward(request, response);
