d0269354d7f142051ef804ad61de6d38d3a32c8e
박지윤 <jiyoo_n@naver.com>
2016-05-02 22:30:59 +0900

    ddd

--- src/com/hb/controller/customer/NoticeEdit.java
+++ src/com/hb/controller/customer/NoticeEdit.java
@@ -14,20 +14,21 @@ import com.hb.model.UserDao;
 
 import bean.NoticeData;
 import bean.UserData;
+import net.sf.json.JSONObject;
      
 @WebServlet("/customer/noticeEdit.do")
 public class NoticeEdit extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		String title = request.getParameter("title");
-//		System.out.println("id넘어옴"+id);
+			String id= request.getParameter("id");
+			
+			NoticeDao dao = new NoticeDao();
+		    NoticeData bean = dao.SelectEdit(id);
+		    
+		    request.setAttribute("noticeDto", bean);
+			request.getRequestDispatcher("/customer/noticeEdit.jsp").forward(request, response);
 		
-		NoticeDao dao = new NoticeDao();
 		
-	    NoticeData bean = dao.SelectEdit(title);
-		
-		request.setAttribute("noticeDto", bean);
-		request.getRequestDispatcher("/customer/noticeEdit.jsp").forward(request, response);
 	}
 	
 }
--- src/com/hb/model/NoticeDao.java
+++ src/com/hb/model/NoticeDao.java
@@ -71,14 +71,14 @@ public class NoticeDao {
 		return bean;
 	}
 
-	public NoticeData SelectEdit(String title) {
+	public NoticeData SelectEdit(String id) {
 		NoticeData bean = new NoticeData();
 		
 		try {
-			sql = "select title, content from TB_NOTICE join TB_USER on TB_NOTICE.id_fk = TB_USER.id where title=?";
+			sql = "select title, content from TB_NOTICE join TB_USER on TB_NOTICE.id_fk = TB_USER.id where id=?";
 			
 			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, title);
+			pstmt.setString(1, id);
 			rs = pstmt.executeQuery();  
 			if (rs.next()) {
 				bean.setTitle(rs.getString(1));
