603704b8c0ebd15b13dad286d52cf8e0f83e18fe
hb6008 <hb6008@hb6008>
2016-05-03 15:06:18 +0900

    ddd

---
+++ src/com/hb/controller/dept/StuDetail.java
@@ -0,0 +1,11 @@
+package com.hb.controller.dept;
+
+public class StuDetail {
+ public static void main(String[] args) {
+	System.out.println("dsfsd");
+	System.out.println("dsfsd");
+	System.out.println("dsfsd");
+	System.out.println("dsfsd");
+	System.out.println("dsfsd");
+}
+}
